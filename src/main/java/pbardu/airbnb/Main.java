9adc24243a910997ba79fb2b0ee35a01d4caaa67
System Administrator <root@cefimmac08.home>
2019-02-12 15:44:22 +0100

    push du dossier original correction

---
+++ src/main/java/pbardu/airbnb/Main.java
@@ -0,0 +1,43 @@
+package pbardu.airbnb;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import pbardu.airbnb.data.AirBnBData;
+import pbardu.airbnb.data.Search;
+import pbardu.airbnb.logements.Logement;
+import pbardu.airbnb.utilisateurs.Voyageur;
+import pbardu.airbnb.reservations.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        Search.SearchBuilder searchBuilder = new Search.SearchBuilder(2).possedeBalcon(false);
+        Search search = searchBuilder.build();
+        ArrayList<Logement> logements = search.result();
+
+        for (Logement logement: logements) {
+            System.out.println("--------------------------");
+            logement.afficher();
+        }
+
+        */
+
+
+        Voyageur voyageur = AirBnBData.getInstance().getVoyageurs().get(0);
+
+        // Critères de mon séjour
+        Date dateArrivee = new Date(1552255232322l);
+        Logement logement = AirBnBData.getInstance().getLogements().get(1);
+        int nbNuits = 2;
+        int nbVoyageurs = 2;
+
+        Sejour sejour = SejourFactory.getSejour(dateArrivee, logement, nbNuits, nbVoyageurs);
+
+        // Réservation
+        Reservation reservation = new Reservation(sejour, voyageur);
+        reservation.afficher();
+    }
+}
---
+++ src/main/java/pbardu/airbnb/reservations/SejourFactory.java
@@ -0,0 +1,21 @@
+package pbardu.airbnb.reservations;
+
+import pbardu.airbnb.logements.Logement;
+
+import java.util.Date;
+
+public class SejourFactory {
+
+    public static Sejour getSejour(Date dateArrivee, Logement logement, int nbNuits, int nbVoyageurs) {
+
+        Sejour sejour;
+
+        if (nbNuits <= 5) {
+            sejour = new SejourCourt(dateArrivee, logement, nbNuits, nbVoyageurs);
+        } else {
+            sejour = new SejourLong(dateArrivee, logement, nbNuits, nbVoyageurs);
+        }
+
+        return sejour;
+    }
+}
